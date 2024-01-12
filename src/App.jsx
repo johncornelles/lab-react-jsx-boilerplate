7fc72a8ef184bf07f1bfc034d76a1b01985acb78
johncornelles <john.cornellews@kalvium.community>
2024-01-12 11:30:01 +0530

    yeah buddy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant
     },
     {
-      id:4,
-      img:elephant
+      id: 4,
+      img: elephant
     }
-  ]
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  const data = imageData();
+
+  return (
+    <div className='gridHolder'>
+      {data.map((ele) => (
+        <img id={ele.id} src={ele.img} alt={`${ele.id}`} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
